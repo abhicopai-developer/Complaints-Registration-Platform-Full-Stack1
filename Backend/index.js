563b59f5418463fb5f012f1ccf045b7719e2f191
Abhishek <abhicopai@gmail.com>
2026-05-17 14:59:26 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -18,16 +18,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
